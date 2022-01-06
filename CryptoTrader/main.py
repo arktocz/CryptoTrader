0fa628e115e96788096814012fe9c468a457b205
arktocz <86154324+arktocz@users.noreply.github.com>
2022-01-06 11:01:34 +0100

    Update main.py

--- CryptoTrader/main.py
+++ CryptoTrader/main.py
@@ -6,6 +6,3 @@ if __name__ == '__main__':
     ws=websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
     ws.run_forever()
 
-
-#RSI =100- (100/ 1+RS)
-#RS= AVG GAIN/AVG LOSS for x candles
